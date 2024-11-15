2718056d35619af5b37f7d12b5fef803d4c847d0
Wizzuize <21522040@gm.uit.edu.vn>
2024-11-15 12:00:47 +0700

    profile pg, chat, etc

---
+++ src/Routes/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import "./landingpage.scss"
+
+function LandingPage () {
+    return (
+        
+    <div className="cover-section">
+                <div className="landing-page-cover">
+                    <img src="/landing_cover.png" alt="Profile Cover" />
+                </div>
+                </div>
+    );
+}
+
+export default LandingPage;
